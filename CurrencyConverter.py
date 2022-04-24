ca1ccaa61a049a79085693af9e2cdc6524bafb18
abuobeid <77234800+abuobeid@users.noreply.github.com>
2022-04-25 00:16:32 +0300

    Update CurrencyConverter.py
    Modifying the codes

--- CurrencyConverter.py
+++ CurrencyConverter.py
@@ -1,28 +1,18 @@
-import urllib.request
-import json
-
-class CurrencyConverter:
-
-	rates = {}
-
-	def __init__(self, url):
-		req = urllib.request.Request(url, headers={'User-Agent': 'howCode Currency Bot'})
-		data = urllib.request.urlopen(req).read()
-		data = json.loads(data.decode('utf-8'))
-		self.rates = data["rates"]
-
-	def convert(self, amount, from_currency, to_currency):
-		initial_amount = amount
-		if from_currency != "EUR":
-			amount = amount / self.rates[from_currency]
-		if to_currency == "EUR":
-			return initial_amount, from_currency, '=', amount, to_currency
-		else:
-			return initial_amount, from_currency, '=', amount * self.rates[to_currency], to_currency
-
-converter = CurrencyConverter("http://howco.de/ex")
-
-print(converter.convert(1.0, "EUR", "USD"))
-print(converter.convert(1.0, "GBP", "USD"))
-print(converter.convert(1.0, "CAD", "GBP"))
-print(converter.convert(1.0, "CAD", "EUR"))
+#!/usr/bin/env python3.7
+# !-*- coding: utf-8 -*-
+#
+# Copyright (c) 2018 jd.com, Inc. All Rights Reserved
+#
+"""
+File:   Moneychange.py
+Author: wangshuai191
+Date:   2019/2/11 20:15
+Brief:  
+"""
+TempStr = input()
+if TempStr[0:3] in ['RMB']:
+    USD = eval(TempStr[3:])/6.78
+    print("USD{:.2f}".format(USD))
+elif TempStr[0:3] in ['USD']:
+    RMB = eval(TempStr[3:])*6.78
+    print("RMB{:.2f}".format(RMB))
